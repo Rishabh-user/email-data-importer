c83138ba425f64abbbebda2c545a9a7c856ae69d
RishabhRawat <rishabh.rawat@aiqmen.ai>
2025-12-21 21:08:33 +0530

    [Settings]

--- data_importer_django/settings.py
+++ data_importer_django/settings.py
@@ -53,6 +53,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    # ✅ ADD THIS
+    'django.middleware.locale.LocaleMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
